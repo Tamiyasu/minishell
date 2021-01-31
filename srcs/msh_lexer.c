41b1691ef8356c3ca26ffc135321d9dc46110786
Tamiyasu Murakami <tmurakam@c1r16s6.42tokyo.jp>
2021-01-31 21:16:53 +0900

    merge retry

--- srcs/msh_lexer.c
+++ srcs/msh_lexer.c
@@ -6,15 +6,7 @@
 /*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 16:38:10 by ysaito            #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2021/01/17 21:01:47 by ysaito           ###   ########.fr       */
-=======
-/*   Updated: 2021/01/24 17:42:42 by ysaito           ###   ########.fr       */
->>>>>>> yuka-dir
-=======
 /*   Updated: 2021/01/26 22:46:58 by ysaito           ###   ########.fr       */
->>>>>>> yuka-dir
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +14,6 @@
 #include "libft.h"
 #include "get_next_line.h"
 
-<<<<<<< HEAD
-void	lexer_lstinit(t_lsttoken *token, int input_len)
-=======
 static void			lexer_structinit(t_lexer *lexer)
 {
 	lexer->data_idx = 0;
@@ -34,7 +23,6 @@ static void			lexer_structinit(t_lexer *lexer)
 }
 
 static void			lexer_lstinit(t_lsttoken *token, int input_len)
->>>>>>> yuka-dir
 {
 	token->data = malloc(sizeof(char *) * (input_len + 1));
 	if (token->data == NULL)
@@ -48,65 +36,6 @@ static t_lsttoken	*lexer_lstnew(int input_len)
 	t_lsttoken	*return_p;
 
 	return_p = malloc(sizeof(t_lsttoken));
-<<<<<<< HEAD
-	if (return_p)
-	{
-		return_p->next = 0;
-		return_p->data = malloc(sizeof(char *) * (input_len + 1));
-		if (return_p->data == NULL)
-			return (NULL);
-	}
-	ft_bzero(return_p->data, input_len);
-	return (return_p);
-}
-
-// static void	lexer_lstadd(t_lsttoken *token, int input_len)
-// {
-// 	while (token->next != NULL)
-// 	{
-// 		token = token->next;
-// 	}
-// 	token->next = lexer_lstnew(input_len);
-// }
-
-
-/*
-** トークン(意味のある単語)に分ける
-*/
-t_lsttoken	*msh_lexer(char *input)
-{
-	t_lsttoken *token;
-	t_lsttoken *savetoken;
-	size_t	input_len;
-	int	state;
-	int	idx;
-	int	data_idx;
-	int	squote;
-
-	//inputの長さを求める
-	input_len = ft_strlen(input);
-	token = lexer_lstnew(input_len);
-	savetoken = token;
-	state = STATE_GENERAL;
-
-	idx = 0;
-	data_idx = 0;
-	squote = 0;
-	//inputを１文字ずつ評価
-	while (input[idx] != '\0')
-	{
-		if (state == STATE_GENERAL)
-		{
-			if (input[idx] == '\'')
-			{
-				state = STATE_IN_SQUOTE;
-				squote++;
-			}
-			//'\""(double quote)がきたら //'$', '\\'以外は文字列リテラルとされる
-			else if (input[idx] == '\"')
-			{
-				state = STATE_IN_DQUOTE;
-=======
 	if (return_p == NULL)
 		return (NULL);
 	lexer_lstinit(return_p, input_len);
@@ -234,76 +163,10 @@ static void	lexer_evaluate_input(t_lsttoken *token, t_lexer *lexer, char *input,
 			{
 				lexer->state = STATE_IN_DQUOTE;
 				lexer->dquote++;
->>>>>>> yuka-dir
 			}
 			else if (input[idx] == '\\')
 			{
 				idx++;
-<<<<<<< HEAD
-				token->data[data_idx++] = input[idx];
-			}
-			else if (input[idx] == ' ' || input[idx] == '\t')
-			{
-				if (data_idx != 0)
-				{
-					token->data[data_idx] = '\0';
-					token->next = malloc(sizeof(t_lsttoken));
-					token = token->next;
-					lexer_lstinit(token, input_len);
-					data_idx = 0;
-				}
-			}
-			//'|', ';', '<', '>', '=' がきたら
-			else if (input[idx] == '|' || input[idx] == ';'
-					|| input[idx] == '<' || input[idx]  == '>' || input[idx] == '=')
-			{
-				if  (data_idx != 0)
-				{
-					token->data[data_idx] = '\0';
-					token->next = malloc(sizeof(t_lsttoken));
-					token = token->next;
-					lexer_lstinit(token, input_len);
-					data_idx = 0;
-				}
-				token->data[0] = input[idx];
-				token->data[1] = '\0';
-
-				token->next = malloc(sizeof(t_lsttoken));
-				token = token->next;
-				lexer_lstinit(token, input_len);
-			}
-			//それ以外の文字
-			else
-			{
-				token->data[data_idx++] = input[idx];
-			}
-		}
-		else if (state == STATE_IN_DQUOTE)
-		{
-			if (input[idx] == CHAR_DQUOTE)
-			{
-				state = STATE_GENERAL;
-				idx++;
-				continue;
-			}
-			token->data[data_idx++] = input[idx];
-		}
-		else if (state == STATE_IN_SQUOTE)
-		{
-			if (input[idx] == CHAR_SQUOTE)
-			{
-				state = STATE_GENERAL;
-				squote++;
-				idx++;
-				continue;
-			}
-			token->data[data_idx++] = input[idx];
-		}
-		idx++;
-	}
-	token->data[data_idx] = '\0';
-	return (savetoken);
-=======
 				token->data[lexer->data_idx++] = input[idx];
 			}
 			else if (input[idx] == ' ' || input[idx] == '\t')
@@ -408,5 +271,4 @@ t_lsttoken	*msh_lexer(char *input)
 		nextinput = NULL;
 	}
 	return (token);
->>>>>>> yuka-dir
 }
