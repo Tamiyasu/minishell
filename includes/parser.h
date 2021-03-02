93afcd9a36b54e7cf73b3b1415dd471c28915f97
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-02 20:29:26 +0900

    fix redirect filecheck

---
+++ includes/parser.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 00:40:42 by tmurakam          #+#    #+#             */
+/*   Updated: 2021/02/28 19:31:26 by tmurakam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# include "minishell.h"
+# include "lexer.h"
+
+enum input_type
+{
+    FT_EMPTY_F,
+    FT_FILENAME_F,
+    FT_REDIRECT_I_F, // <
+    FT_REDIRECT_O_F, // >  "254>"
+    FT_REDIRECT_A_F, // >> "3>>"
+    FT_COMMAND_F,
+    FT_PIPE_F,
+    FT_SEMICOLON_F
+};
+
+typedef struct	s_parser_node
+{
+    struct	s_parser_node *l_node;
+    struct	s_parser_node *r_node;
+    t_lsttoken *content;
+}				t_parser_node;
+
+t_parser_node   *parser(t_lsttoken *token_list);
+t_parser_node   *free_tree(t_parser_node **node);
+
+#endif
