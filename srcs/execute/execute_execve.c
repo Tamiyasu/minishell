3e34dfd2cb219554460887ff1d806ef1da0f4f6a
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-02 23:44:11 +0900

    remove some print for debug

--- srcs/execute/execute_execve.c
+++ srcs/execute/execute_execve.c
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 14:44:34 by ysaito            #+#    #+#             */
-/*   Updated: 2021/03/02 23:21:58 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/02 23:26:57 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,7 +137,7 @@ static int	execve_execute_command(char *command, char **args, t_env *env)
 	}
 	if (pid == 0) /* 以下のコードが子プロセスで実行される */
 	{
-		printf("in child pid=[%d]\n", getpid());//del
+		//printf("in child pid=[%d]\n", getpid());//del
 		// printf("子プロセスの中身 (command : %s)\n", command);
 		/*
 		int i = 0;
@@ -154,32 +154,22 @@ static int	execve_execute_command(char *command, char **args, t_env *env)
 		}
 		*/
 
-		int __r = execve(command, args, env->data);
-		printf("__r : %d\n", __r);
-
-		if (__r == -1)//コマンドの実行エラー
+		if (execve(command, args, env->data) == -1)//コマンドの実行エラー
 		{
-			printf("失敗時 : errno : %d\n", errno);
 			ft_putendl_fd(strerror(errno), 1);
 			exit(errno);
 		}
 	}
 	else /* 以下のコードが親プロセスで実行される  */
 	{
-		printf("in parent childpid=[%d]\n", pid);//del
-		int __k = waitpid(pid, &pid_status, 0);
-		printf("in parent pid_status=[%d]\n", pid_status);//del
-		printf("in parent __k=[%d]\n", __k);//del
-		/*if((__k == -1))
+		//printf("in parent childpid=[%d]\n", pid);//del
+		if((waitpid(pid, &pid_status, 0) == -1))
 		{
 			ft_putendl_fd(strerror(errno), 1);
 			return (errno);
-		}*/
-		printf("in parent pid_status=[%d]\n", pid_status);//del
+		}
 		//printf("the child pid=[%d]::pid_status=[%d][%d][%d][%d][%d][%d][%d][%d]\n", pid, WIFEXITED(pid_status), WEXITSTATUS(pid_status), WIFSIGNALED(pid_status), WTERMSIG(pid_status), WCOREDUMP(pid_status),  WIFSTOPPED(pid_status), WSTOPSIG(pid_status), WIFCONTINUED(pid_status));//del
 	}
-	printf("WIFEXITED(pid_status) : %d\n", WIFEXITED(pid_status));
-	printf("WEXITSTATUS(pid_status) : %d\n", WEXITSTATUS(pid_status));
 	return (WEXITSTATUS(pid_status));
 }
 
@@ -189,7 +179,6 @@ int	execve_output_error(t_lsttoken *token, char *error_str, int exit_status)
 	ft_putstr_fd(token->data, 1);
 	ft_putstr_fd(": ", 1);
 	ft_putendl_fd(error_str, 1);
-	print_token(token, "here 6 ******** ");
 	return (exit_status);
 }
 
@@ -211,40 +200,31 @@ int			execute_execve(t_lsttoken *token, t_env *env)
 	}
 	while (1)
 	{
-		print_token(token, "here");
 		command = execve_format_command(token, env_path, path_idx);
 		args = execve_format_args(token, command);
 		execve_rc = execve_execute_command(command, args, env);
-		print_token(token, "here 0 ******** ");
 		free(command);
-		print_token(token, "here 1 ******** ");
 		free_args(args);
 		if (execve_rc == 0) /*work fine*/
 		{
-			print_token(token, "here 2 ******** ");
 			free_args(env_path);
 			return (0);
 		}
 		if (execve_rc != 2) /*error but return code is not 2*/
 		{
-			print_token(token, "here 3 ******** ");
-			printf("execve_rc : %d\n", execve_rc);
 			free_args(env_path);
-			print_token(token, "here 3' ******** ");
 			//return (execve_output_error(token, strerror(execve_rc), execve_rc));
 			return (execve_rc);
 		} 
 		/* No such file or directory */
 		if (relative_path == 0)
 		{
-			print_token(token, "here 4 ******** ");
 			free_args(env_path);
 			return (execve_output_error(token, strerror(execve_rc), 127));
 		}
 		path_idx++;
 		if (env_path[path_idx] == NULL)
 		{
-			print_token(token, "here 5 ******** ");
 			free_args(env_path);
 			return (execve_output_error(token, "command not found", 127));
 		}
