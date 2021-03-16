344e7f8a13c0eb90901549afd19e520c365b224a
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-16 22:25:11 +0900

    先頭に パイプが来ても大丈夫なように修正。

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 00:56:58 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/15 23:22:53 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/16 22:23:39 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,7 +132,7 @@ t_parser_node   *find_command_node(t_parser_node *node)
     }
     else if (is_redirect(node->content->flag))
     {
-        while(node->content->flag != FT_COMMAND_F)
+        while(node && node->content && node->content->flag != FT_COMMAND_F)
         {
             node = node->l_node;
         }
@@ -145,8 +145,9 @@ t_parser_node   *find_parent_node(t_parser_node   *node)
 {
     t_parser_node *ret_node;
     ret_node = NULL;
+    printf("node_p : %p\n", node);
 
-    if  (node->content->flag == FT_PIPE_F || node->content->flag == FT_SEMICOLON_F)
+    if (node && node->content && (node->content->flag == FT_PIPE_F || node->content->flag == FT_SEMICOLON_F))
     {
         if(node->r_node)
         {
@@ -263,6 +264,7 @@ t_parser_node   *parser(t_lsttoken *token_list)
         }
         else if(is_redirect(c_type))
         {
+            printf("here\n");
             new_node = malloc(sizeof(t_parser_node));
             new_node->content = token;
             new_node->r_node = NULL;
