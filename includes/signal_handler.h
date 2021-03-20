2740f1229748bda6d905df73bfd8865eb11a06c0
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-20 13:53:37 +0900

    for norm

--- includes/signal_handler.h
+++ includes/signal_handler.h
@@ -6,20 +6,19 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 14:45:39 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/20 13:51:58 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/20 13:53:34 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include "minishell.h"
-
 #ifndef SIGNAL_HANDLER_H
 # define SIGNAL_HANDLER_H
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include "minishell.h"
 
-pid_t   c_pid(pid_t t);
-void    sig_handler_c(int signo);
-void    sig_handler_p(int signo);
+pid_t	c_pid(pid_t t);
+void	sig_handler_c(int signo);
+void	sig_handler_p(int signo);
 #endif
