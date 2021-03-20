44b5d47fb912a04e91d56d8da264f1adc518e041
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-20 12:40:40 +0900

    for norm

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 00:56:58 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/20 12:35:29 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/20 12:40:28 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,8 +55,7 @@ t_parser_node	*find_c_node(t_parser_node *node)
 	ret_node = NULL;
 	if (node->content == NULL || node->content->flag == FT_COMMAND_F)
 		ret_node = node;
-	else if (node->content->flag == FT_PIPE_F
-			|| node->content->flag == FT_SEMICOLON_F)
+	else if (node->content->flag == FT_PIPE_F || node->content->flag == FT_SEMICOLON_F)
 	{
 		if (node->r_node)
 		{
