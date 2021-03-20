98c9e823f0a184521464d69bfdd221201efe71aa
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-20 13:39:44 +0900

    for norm

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 00:40:42 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/20 13:37:24 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/20 13:39:41 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,29 @@
 # include "minishell.h"
 # include "lexer.h"
 
-enum    input_type
+enum	input_type
 {
-    FT_EMPTY_F,
-    FT_FILENAME_F,
-    FT_REDIRECT_I_F,
-    FT_REDIRECT_O_F,
-    FT_REDIRECT_A_F,
-    FT_COMMAND_F,
-    FT_PIPE_F,
-    FT_SEMICOLON_F
+	FT_EMPTY_F,
+	FT_FILENAME_F,
+	FT_REDIRECT_I_F,
+	FT_REDIRECT_O_F,
+	FT_REDIRECT_A_F,
+	FT_COMMAND_F,
+	FT_PIPE_F,
+	FT_SEMICOLON_F
 };
 
-typedef struct  s_parser_node
+typedef struct	s_parser_node
 {
-    struct	s_parser_node *l_node;
-    struct	s_parser_node *r_node;
-    t_token *content;
+	struct s_parser_node	*l_node;
+	struct s_parser_node	*r_node;
+	t_token					*content;
 }				t_parser_node;
 
 t_parser_node	*free_tree(t_parser_node **node);
 t_parser_node	*make_node(t_token *con, t_parser_node *l, t_parser_node *r);
-int             parser(t_token *token_list, t_parser_node **node_p);
-void            node_print(t_parser_node *node, int deepness);
+int				parser(t_token *token_list, t_parser_node **node_p);
+void			node_print(t_parser_node *node, int deepness);
 t_parser_node	*find_c_node(t_parser_node *node);
 int				is_redirect(int flag);
 int				check_input(int c_type, int last_type);
