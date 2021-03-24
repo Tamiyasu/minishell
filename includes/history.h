8c097ba41304d76496ae8582e313cc0104874af1
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-25 00:19:07 +0900

    make t_hist for save two strs for hist and edited hist

--- includes/history.h
+++ includes/history.h
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 23:59:30 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/24 00:01:25 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/25 00:15:45 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 # define HISTORY_H
 # include "libft.h"
 
-char    *history(char *str, int f);
+typedef struct	s_hist
+{
+	char			*hist_str;
+	char			*tmp_str;
+	struct s_hist	*next;
+}				t_hist;
 
+char    *history(char *str, int f);
 
 #endif
--- srcs/history/history.c
+++ srcs/history/history.c
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 23:58:07 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/24 00:29:52 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/25 00:17:13 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void set(char *str, int i, t_list *h_list)
     int k;
 
     k = 0;
-    while (++k < i && h_list)
+    while (k++ < i && h_list)
         h_list = h_list->next;
     if (h_list)
     {
@@ -38,21 +38,34 @@ static char *get(int i, t_list *h_list)
 char    *history(char *str, int f)
 {
     static int      i;
-    static t_list   *h_list;
-    t_list          *tmp_list;
+    static int      s;
+    static t_hist   *h_list;
+    t_hist          *tmp_list;
     char            *ret_str;
 
-    if(i)
-        set(str, i, h_list);
+    if(str == NULL)
+    {
+        ft_lstclear(&h_list, free);
+        return (NULL);
+    }
+    if(s == 0)
+    {
+        s = 1;
+        tmp_list = malloc(sizeof(t_hist));
+        tmp_list->hist_str = ft_strdup(str);
+        tmp_list->tmp_str = NULL;
+        ft_lstadd_front(&h_list, tmp_list);
+    }
+    set(str, i, h_list);
     if (f == 0)
     {
         i = 0;
-        tmp_list = malloc(sizeof(t_list));
-        tmp_list->content = ft_strdup(str);
-        ft_lstadd_front(&h_list, tmp_list);
+        s = 0;
         ret_str = NULL;
     }
-    else 
+    else
+        i += f;
+        i = i < 0 ? 0 : i;
         ret_str = ft_strdup(get(i, h_list));
     return (ret_str);
 }
