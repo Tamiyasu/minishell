b85f68ffbb45764eaae9fb2eb16b645480e3f133
ysaito <shiva.lts239@gmail.com>
2021-03-26 20:04:02 +0900

    backslash

--- srcs/expansion/exps_quote.c
+++ srcs/expansion/exps_quote.c
@@ -6,7 +6,7 @@
 /*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 20:06:37 by ysaito            #+#    #+#             */
-/*   Updated: 2021/03/23 15:17:27 by ysaito           ###   ########.fr       */
+/*   Updated: 2021/03/26 19:58:55 by ysaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,25 @@ char
 	new_data = save_reading_data(token->data, new_data, data);
 	while (token->data[data->idx] != '\"')
 	{
-		if (token->data[data->idx] == '$' && token->data[data->idx + 1]
+		if (token->data[data->idx] == '\\'
+			&& (token->data[data->idx + 1] == '\\'
+			|| token->data[data->idx + 1] == '\"'
+			|| token->data[data->idx + 1] == '$'))
+		{
+			{
+				data_increment(data);
+				data->start = data->idx;
+				new_data = save_reading_data(token->data, new_data, data);
+			}
+		}
+		else if (token->data[data->idx] == '$' && token->data[data->idx + 1]
 			&& (ft_isalpha(token->data[data->idx + 1])
 			|| token->data[data->idx + 1] == '_'
 			|| token->data[data->idx + 1] == '?'))
 			new_data = exps_set_envdata(token, new_data, data, env);
 		else
 			data_increment(data);
+		printf("token->data[%d]=[%c]\n", data->idx, token->data[data->idx]);
 	}
 	new_data = save_reading_data(token->data, new_data, data);
 	return (new_data);
--- srcs/lexer/lexer_quote.c
+++ srcs/lexer/lexer_quote.c
@@ -6,7 +6,7 @@
 /*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 17:17:38 by ysaito            #+#    #+#             */
-/*   Updated: 2021/03/20 15:08:28 by ysaito           ###   ########.fr       */
+/*   Updated: 2021/03/26 19:59:33 by ysaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,23 @@ int	lexer_count_quote(t_data *data, char *input, char quote)
 	int	save_idx;
 
 	save_idx = data->idx;
+	data_increment(data);
 	while (input[data->idx] != '\0')
 	{
-		data_increment(data);
-		if (input[data->idx] == quote)
+		if (quote == '\"' &&
+			((input[data->idx] == '\\' && input[data->idx + 1] == '\"')
+			|| (input[data->idx] == '\\' && input[data->idx + 1] == '\\')))
+		{
+			data_increment(data);
+			data_increment(data);
+		}
+		else if (input[data->idx] == quote)
 		{
 			data_increment(data);
 			return (1);
 		}
+		else
+			data_increment(data);
 	}
 	data->idx = save_idx;
 	return (0);
