90c1e5d06460a6cb2b05564e81f8628862feb869
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-27 10:37:04 +0900

    retry to make make_history

--- srcs/history/history.c
+++ srcs/history/history.c
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 23:58:07 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/27 10:19:10 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/27 10:36:39 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,33 +98,38 @@ char	*history_arrange(int *i, int f, t_list *h_list)
 	return(ft_strdup(get(*i, h_list)));
 }
 
+char	*history_destroy(t_list **h_list)
+{
+	ft_lstclear(h_list, free_hist);
+	return (NULL);
+}
+
+char	*history_make(int *i, char *str, t_list **h_list)
+{
+	if (*i != 0 )
+		reset(*i, *h_list);
+	set(str, 0, *h_list);
+	*i = 0;
+	if (ft_strlen(str) != 0)
+		add_new_hist(h_list);
+	return (NULL);
+}
+
 char    *history(char *str, int f)
 {
 	static int		i;
 	static t_list	*h_list;
-	//t_list			*tmp_list;
 	char			*ret_str;
 
 	printf("\nin history : str = [%s], f=[%d]\n", str, f);
 
 	if(str == NULL)
-	{
-		ft_lstclear(&h_list, free_hist);
-		return (NULL);
-	}
+		return history_destroy(&h_list);
 	if(h_list == NULL)
 		add_new_hist(&h_list);
 	set(str, i, h_list);
 	if (f == 0)
-	{
-		if (i != 0 )
-			reset(i, h_list);
-		set(str, 0, h_list);
-		i = 0;
-		if (ft_strlen(str) != 0)
-			add_new_hist(&h_list);
-		ret_str = NULL;
-	}
+		history_make(&i, str, &h_list);
 	else
 		ret_str = history_arrange(&i, f, h_list);
 	print_hist(h_list);
