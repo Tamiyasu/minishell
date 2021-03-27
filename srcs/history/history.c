141ad165b1036b0fe073a73efdb723631159c34e
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-27 09:32:29 +0900

    forbit move histly list through max of histly list size

--- srcs/history/history.c
+++ srcs/history/history.c
@@ -6,20 +6,20 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 23:58:07 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/26 20:22:19 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/27 09:31:10 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "history.h"
 #include <stdio.h>
 
-static void print_hist(t_list *h_hist)
+static void print_hist(t_list *h_list)
 {
-	printf("here - %p\n", h_hist);
-	while(h_hist)
+	printf("here - %p\n", h_list);
+	while(h_list)
 	{
-		printf("h_hist[%p] : h_hist->content[%p], h_hist->content->hist_str=[%s], h_hist->content->tmp_str=[%s]\n", h_hist, h_hist->content, ((t_hist *)(h_hist->content))->hist_str, ((t_hist *)(h_hist->content))->tmp_str);
-		h_hist = h_hist->next;
+		printf("h_list[%p] : h_list->content[%p], h_list->content->hist_str=[%s], h_list->content->tmp_str=[%s]\n", h_list, h_list->content, ((t_hist *)(h_list->content))->hist_str, ((t_hist *)(h_list->content))->tmp_str);
+		h_list = h_list->next;
 	}
 }
 
@@ -42,7 +42,7 @@ static void set(char *str, int i, t_list *h_list)
 {
 	int k;
 
-	printf("in history set : i=[%d] : str=[%s]\n", i, str);
+	//printf("in history set : i=[%d] : str=[%s]\n", i, str);
 	k = 0;
 	while (k++ < i && h_list)
 		h_list = h_list->next;
@@ -56,13 +56,13 @@ static void set(char *str, int i, t_list *h_list)
 			((t_hist *)(h_list->content))->hist_str = ft_strdup(str);
 		}
 	}
-
 }
+
 static char *get(int i, t_list *h_list)
 {
 	int k;
 
-	printf("in history get : i=[%d]\n", i);
+	//printf("in history get : i=[%d]\n", i);
 	k = 0;
 	while (k++ < i && h_list->next)
 		h_list = h_list->next;
@@ -79,30 +79,40 @@ void    free_hist(void *hist)
 	free(hist);
 }
 
+void	add_new_hist(t_list **h_list)
+{
+	t_list			*tmp_list;
+
+	tmp_list = malloc(sizeof(t_list));
+	tmp_list->content = malloc(sizeof(t_hist));
+	((t_hist *)(tmp_list->content))->hist_str = NULL;
+	((t_hist *)(tmp_list->content))->tmp_str = NULL;
+	ft_lstadd_front(h_list, tmp_list);
+}
+
 char    *history(char *str, int f)
 {
-	static int      i;
-	static int      s;
-	static t_list   *h_list;
-	t_list          *tmp_list;
-	char            *ret_str;
+	static int		i;
+	static t_list	*h_list;
+	//t_list			*tmp_list;
+	char			*ret_str;
 
-	printf("\nin history : str = [%s], f=[%d]\n", str, f);
+	//printf("\nin history : str = [%s], f=[%d]\n", str, f);
 
 	if(str == NULL)
 	{
 		ft_lstclear(&h_list, free_hist);
 		return (NULL);
 	}
-	if(s == 0)
-	{
-		s = 1;
+	if(h_list == NULL)
+		add_new_hist(&h_list);
+/*	{
 		tmp_list = malloc(sizeof(t_list));
 		tmp_list->content = malloc(sizeof(t_hist));
 		((t_hist *)(tmp_list->content))->hist_str = ft_strdup(str);
 		((t_hist *)(tmp_list->content))->tmp_str = NULL;
 		ft_lstadd_front(&h_list, tmp_list);
-	}
+	}*/
 	set(str, i, h_list);
 	if (f == 0)
 	{
@@ -110,18 +120,24 @@ char    *history(char *str, int f)
 			reset(i, h_list);
 		set(str, 0, h_list);
 		i = 0;
-		s = 0;
-
+		if (ft_strlen(str) != 0)
+			add_new_hist(&h_list);
+/*		{
+			tmp_list = malloc(sizeof(t_list));
+			tmp_list->content = malloc(sizeof(t_hist));
+			((t_hist *)(tmp_list->content))->hist_str = ft_strdup(str);
+			((t_hist *)(tmp_list->content))->tmp_str = NULL;
+			ft_lstadd_front(&h_list, tmp_list);
+		}*/
 		ret_str = NULL;
 	}
 	else
 	{
 		i += f;
 		i = i < 0 ? 0 : i;
+		i = i < ft_lstsize(h_list) ? i : ft_lstsize(h_list);
 		ret_str = ft_strdup(get(i, h_list));
 	}
-	//if(!ret_str)
-	//    ret_str = ft_strdup("empty history!");
 	print_hist(h_list);
 	return (ret_str);
 }
