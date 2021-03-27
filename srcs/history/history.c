ab501a5b5a231cfbd7fe7f3e866584410c99172d
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-03-27 10:06:00 +0900

    forbit move histly limit + 1

--- srcs/history/history.c
+++ srcs/history/history.c
@@ -6,7 +6,7 @@
 /*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 23:58:07 by tmurakam          #+#    #+#             */
-/*   Updated: 2021/03/27 09:31:10 by tmurakam         ###   ########.fr       */
+/*   Updated: 2021/03/27 10:05:30 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,54 +90,49 @@ void	add_new_hist(t_list **h_list)
 	ft_lstadd_front(h_list, tmp_list);
 }
 
+char	*history_arrange(int *i, int f, t_list *h_list)
+{
+	int lstsize;
+
+	lstsize = ft_lstsize(h_list);
+	*i += f;
+	*i = *i < 0 ? 0 : *i;
+	*i = *i < (lstsize - 1) ? *i : (lstsize - 1);
+	return (get(*i, h_list));
+}
+
+char	*history_make(int *i, char *str, t_list *h_list)
+{
+	if (*i != 0 )
+		reset(*i, h_list);
+	set(str, 0, h_list);
+	*i = 0;
+	if (ft_strlen(str) != 0)
+		add_new_hist(&h_list);
+	return (NULL);
+}
+
+char	*history_destroy(t_list **h_list)
+{
+	ft_lstclear(h_list, free_hist);
+	return (NULL);
+}
+
 char    *history(char *str, int f)
 {
 	static int		i;
 	static t_list	*h_list;
-	//t_list			*tmp_list;
 	char			*ret_str;
 
-	//printf("\nin history : str = [%s], f=[%d]\n", str, f);
-
-	if(str == NULL)
-	{
-		ft_lstclear(&h_list, free_hist);
-		return (NULL);
-	}
 	if(h_list == NULL)
 		add_new_hist(&h_list);
-/*	{
-		tmp_list = malloc(sizeof(t_list));
-		tmp_list->content = malloc(sizeof(t_hist));
-		((t_hist *)(tmp_list->content))->hist_str = ft_strdup(str);
-		((t_hist *)(tmp_list->content))->tmp_str = NULL;
-		ft_lstadd_front(&h_list, tmp_list);
-	}*/
+	if(str == NULL)
+		history_destroy(&h_list);
 	set(str, i, h_list);
 	if (f == 0)
-	{
-		if (i != 0 )
-			reset(i, h_list);
-		set(str, 0, h_list);
-		i = 0;
-		if (ft_strlen(str) != 0)
-			add_new_hist(&h_list);
-/*		{
-			tmp_list = malloc(sizeof(t_list));
-			tmp_list->content = malloc(sizeof(t_hist));
-			((t_hist *)(tmp_list->content))->hist_str = ft_strdup(str);
-			((t_hist *)(tmp_list->content))->tmp_str = NULL;
-			ft_lstadd_front(&h_list, tmp_list);
-		}*/
-		ret_str = NULL;
-	}
+		ret_str = history_make(&i, str, h_list);
 	else
-	{
-		i += f;
-		i = i < 0 ? 0 : i;
-		i = i < ft_lstsize(h_list) ? i : ft_lstsize(h_list);
-		ret_str = ft_strdup(get(i, h_list));
-	}
+		ret_str = ft_strdup(history_arrange(&i, f, h_list));
 	print_hist(h_list);
 	return (ret_str);
 }
