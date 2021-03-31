42a774359daf7ad3e581edfd5c75fc38fb1d0145
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2021-04-01 01:06:19 +0900

    move dir by re_constracted full path

--- srcs/execute/command_cd.c
+++ srcs/execute/command_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 20:41:38 by ysaito            #+#    #+#             */
-/*   Updated: 2021/03/31 14:06:37 by ysaito           ###   ########.fr       */
+/*   Updated: 2021/04/01 01:05:15 by tmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,16 +75,97 @@ int		cd_home(t_env *env)
 	return (EXIT_SUCCESS);
 }
 
+int		arr_size(char **null_ended_strs)
+{
+	int ret;
+	ret = 0;
+	while (*null_ended_strs++)
+		ret++;
+	return (ret);
+}
+
+char	*strs_join(char **strs, char *enc)
+{
+	char *str;
+	char *tmp;
+
+	str = ft_strjoin("", "");
+	while (*strs)
+	{
+		tmp = str;
+		str = ft_strjoin(tmp, enc);
+		free(tmp);
+		tmp = str;
+		str = ft_strjoin(tmp, *(strs++));
+		free(tmp);
+	}
+	return (str);
+}
+
+char	*get_aim_dir(t_env *env, char *cd_str)
+{
+	char	**cds;
+	char	**a_cds;
+	char	*str;
+	int		size;
+	int		i;
+	int		j;
+	char	**e_cds;
+
+	str = cwd_wrapper(env, NULL);
+	cds = ft_split(str, '/');
+	a_cds = ft_split(cd_str, '/');
+	size = arr_size(cds) + arr_size(a_cds);
+	e_cds = ft_calloc(sizeof(char *), size + 1);
+	i = 0;
+	while(*(cds + i))
+	{
+		*(e_cds + i) = ft_strdup(*(cds + i));
+		i++;
+	}
+	j = 0;
+	while(*(a_cds + j))
+	{
+		if	(!ft_strcmp(*(a_cds + j), ".."))
+		{
+			printf("----------\n");
+			i--;
+			free(*(e_cds + i));
+			*(e_cds + i) = NULL;
+			j++;
+		}
+		else if (!ft_strcmp(*(a_cds + j), "."))
+		{
+			j++;
+		}
+		else
+		{
+			*(e_cds + i) = ft_strdup(*(a_cds + j));
+			i++;
+			j++;
+		}
+	}
+	str = strs_join(e_cds, "/");
+	printf("%s\n", str);
+	free_args(cds);
+	free_args(a_cds);
+	free_args(e_cds);
+	return (str);
+}
+
+
 int		command_cd(t_token *token, t_env *env)
 {
 	char		*err_str;
+	char		*aim_dir;
 	//struct stat	stat_buf;
 	//int			err_cord;
 
 	token = token->next;
 	if (token == NULL)
 		return (cd_home(env));
-	else if (chdir(token->data) == -1)
+	aim_dir = get_aim_dir(env, token->data);
+	if (chdir(aim_dir) == -1)
 	{
 		//err_cord = ENOENT;
 		// if (!stat(token->data, &stat_buf)
@@ -95,6 +176,7 @@ int		command_cd(t_token *token, t_env *env)
 		free(err_str);
 		return (EXIT_FAILURE);
 	}
+	free(aim_dir);
 	cd_update_envpwd(env, token->data);
 	return (EXIT_SUCCESS);
 }
