a22be4b57089719ad3611015e613103b15aba1f5
ysaito <shiva.lts239@gmail.com>
2021-04-02 14:22:53 +0900

    fix OLDPWD start data

--- srcs/env_set_data.c
+++ srcs/env_set_data.c
@@ -6,7 +6,7 @@
 /*   By: ysaito <ysaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 13:22:55 by ysaito            #+#    #+#             */
-/*   Updated: 2021/04/02 09:15:32 by ysaito           ###   ########.fr       */
+/*   Updated: 2021/04/02 14:20:08 by ysaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,22 @@ int		env_check_data(t_env *env, char **envp)
 	env_num = 0;
 	while (envp[env_num] != NULL)
 		env_num++;
-	//env->oldpwd_flag = env_search(envp, "OLDPWD");//存在するかどうか
+	// env->oldpwd_flag = env_search(envp, "OLDPWD");//存在するかどうか
 	// if (env->oldpwd_flag == -1)
 	// 	env_num++;
+	// if (env_search(envp, "OLDPWD") != -1)
+	// 	env_num--;
+	env->oldpwd_flag = env_search(envp, "OLDPWD");//存在するかどうか
+	if (env->oldpwd_flag != -1)
+		env_num--;
 	env->pwd_flag = env_search(envp, "PWD");
 	if (env->pwd_flag == -1)
 		env_num++;
+	else
+	{
+		if (env->oldpwd_flag != -1 && (env->pwd_flag > env->oldpwd_flag))
+			env->pwd_flag--;
+	}
 	env->shlvl_flag = env_search(envp, "SHLVL");
 	if (env->shlvl_flag == -1)
 		env_num++;
@@ -45,15 +55,15 @@ int		set_shlvl_num(int shlvl_num)
 	return (shlvl_num);
 }
 
-void	env_set_shlvl(t_env *env, int *idx)
+void	env_set_shlvl(t_env *env, int *data_idx)
 {
 	int		shlvl_num;
 	char	*shlvl_str;
 
 	if (env->shlvl_flag == -1)
 	{
-		env->data[*idx] = ft_strdup("SHLVL=1");
-		*idx = *idx + 1;
+		env->data[*data_idx] = ft_strdup("SHLVL=1");
+		*data_idx = *data_idx + 1;
 		return ;
 	}
 	shlvl_num = ft_atoi(&env->data[env->shlvl_flag][6]);
@@ -77,9 +87,13 @@ int		env_set_pwd(t_env *env, int idx)
 	// 	env->data[idx++] = ft_strdup("OLDPWD");
 	env_update_pwddata(env, NULL);
 	if (env->pwd_flag == -1 && env->pwd_data)
+	{
+		printf("in\n");//del
 		env->data[idx++] = ft_strjoin("PWD=", env->pwd_data);
+	}
 	else
 	{
+		printf("env->pwd_flag2[%d]\n", env->pwd_flag);//del
 		tmp_cwd = env->data[env->pwd_flag];
 		if (env->pwd_data)
 		{
@@ -92,24 +106,42 @@ int		env_set_pwd(t_env *env, int idx)
 
 void	env_set_data(t_env *env, char **envp)
 {
+	int	data_idx;
 	int	idx;
 
 	env->num = env_check_data(env, envp);
 	env->data = malloc(sizeof(char *) * (env->num + 1));
 	if (env->data == NULL)
 		ft_enomem();
+	data_idx = 0;
 	idx = 0;
 	while (envp[idx] != NULL)
 	{
-		env->data[idx] = ft_strdup(envp[idx]);
+		// if (!((ft_strncmp(envp[idx], "OLDPWD", 6) == 0) && envp[idx][6] == '='))
+		// {
+		// 	printf("check OLDPWD=[%s]\n", envp[idx]);
+		// 	idx++;
+		// 	continue ;
+		if (idx != env->oldpwd_flag)
+		{
+			env->data[data_idx] = ft_strdup(envp[idx]);
+			printf("check env->data=[%d][%s]\n", data_idx, env->data[data_idx]);//del
+			data_idx++;
+		}
 		idx++;
 	}
-	idx = env_set_pwd(env, idx);
+	data_idx = env_set_pwd(env, data_idx);
+	//
+	for (int x = 0; x < env->num; x++)
+	{
+		printf("check env->data=[%d][%s]\n", x, env->data[x]);//del
+	}
+	//
 	if (0 < ft_strlen(error_str("")))
 		ft_putendl_fd(error_str("shell-init: "), STDERR_FILENO);
 	error_str(NULL);
-	env_set_shlvl(env, &idx);
-	env->oldpwd_flag = 1;
+	env_set_shlvl(env, &data_idx);
+	//env->oldpwd_flag = 1;本当にいらない？
 	env->pwd_flag = 1;
-	env->data[idx] = NULL;
+	env->data[data_idx] = NULL;
 }
